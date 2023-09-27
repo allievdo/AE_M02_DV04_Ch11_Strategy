a71396ef7c4c4d94e1da69b5cd2fd8cb2e204cfe
allikitten <allison.evdokimo@cox.net>
2023-09-26 22:42:48 -0700

    :D

---
+++ AE_M02_DV04_Strategy/Assets/Scripts/FallbackManeuver.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Chapter.Strategy
+{
+    public class FallbackManeuver : MonoBehaviour, IManeuverBehaviour
+    {
+        public void Maneuver(Drone drone)
+        {
+            StartCoroutine(Fallback(drone));
+        }
+
+        IEnumerator Fallback(Drone drone)
+        {
+            float time = 0f;
+            float speed = drone.speed;
+            Vector3 startPosition = drone.transform.position;
+            Vector3 endPosition = startPosition;
+            endPosition.z = drone.fallbackDistance;
+
+            while (time < speed)
+            {
+                drone.transform.position = Vector3.Lerp(startPosition, endPosition, time / speed);
+
+                time += Time.deltaTime;
+
+                yield return null;
+            }
+        }
+    }
+}
